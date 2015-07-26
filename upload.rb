ba0af310bd4193de4eabce935f11f4f377a72a81
Stefan <stefan@whirl-i-gig.com>
2015-07-25 20:35:23 -0400

    Build video file path from json file name and also move json+mp4 file out of the way if things went ok.
    TODO: logging!

--- upload.rb
+++ upload.rb
@@ -12,15 +12,19 @@ config = YAML.load_file(File.dirname(__FILE__) + File::SEPARATOR + 'config.yml')
 # load directory contents
 Dir.glob(config['local_directory'] + File::SEPARATOR + '*.json') do |item|
   json = JSON.parse(File.open(item, 'r').read)
+  media_file_name = File.basename(item, '.json') + '.mp4'
+  media_file_path = config['local_directory'] + File::SEPARATOR + media_file_name
+
+  raise 'video file for ' + item + ' is missing' unless File.exists?(media_file_path)
 
   # upload video file to alhalqa server
   Net::SCP.start(ENV['SSH_HOST'], ENV['SSH_USER'], keys: [ENV['SSH_PRIVATE_KEY']], port: ENV['SSH_PORT']) do |scp|
-    puts 'SCP copy started for ' + json['videofile']
-    scp.upload!(json['videofile'], config['upload_directory'])
+    puts 'SCP copy started for ' + media_file_name
+    scp.upload!(media_file_path, config['upload_directory'])
   end
 
-  #remote_media_path = config['upload_directory'] + File::SEPARATOR + File.basename(json['videofile'])
-  remote_media_path = '/tmp/07-24-2015-14-24-12-recording.mp4'
+  remote_media_path = config['upload_directory'] + File::SEPARATOR + media_file_name
+  #remote_media_path = '/Users/stefan/07-24-2015-14-24-12-recording.mp4'
 
   # create new object representation
   rep = CollectiveAccess.put hostname: config['hostname'], table_name: 'ca_object_representations', endpoint: 'item',
@@ -36,10 +40,11 @@ Dir.glob(config['local_directory'] + File::SEPARATOR + '*.json') do |item|
                                }
                              ]
                            }
-  rep_id = rep['representation_id']
-  raise 'Representation upload seems to have failed' unless rep_id
+
+  raise 'Representation upload seems to have failed' unless rep['representation_id']
 
   # create new object
+  # @todo actually catalog relevant info from json
   obj = CollectiveAccess.put hostname: config['hostname'], table_name: 'ca_objects', endpoint: 'item',
                              request_body: {
                                intrinsic_fields: {
@@ -55,10 +60,14 @@ Dir.glob(config['local_directory'] + File::SEPARATOR + '*.json') do |item|
                                related: {
                                  ca_object_representations: [
                                    {
-                                     representation_id: rep_id
+                                     representation_id: rep['representation_id']
                                    }
                                  ]
                                }
                              }
-  puts obj.inspect
+  raise 'Creating the object seems to have failed' unless obj['object_id']
+
+  # move json and mp4 out of the spooling dir
+  File.rename media_file_path, config['finished_directory'] + File::SEPARATOR + media_file_name
+  File.rename item, config['finished_directory'] + File::SEPARATOR + File.basename(item)
 end
